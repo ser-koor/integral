a165a8dc435d84400a01ea66f2408a6adf2cb7e8
ser-koor <1684398809@qq.com>
2022-11-27 22:57:48 +0800

    样式初始化

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <header>首123页</header>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+
+}
+</script>
+
+<style lang="less" scoped>
+header {
+  background-color: pink;
+}
+</style>
