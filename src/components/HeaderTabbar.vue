9fce6d65e339a70777df56dd1239acfd0d52a537
ser-koor <1684398809@qq.com>
2022-11-30 10:07:17 +0800

    头部样式

--- src/components/HeaderTabbar.vue
+++ src/components/HeaderTabbar.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>header</div>
+  <div class="header wrap">
+    <h1>
+      <img src="../assets/img/indexLogo.6f8ac4f0.png" alt="">
+    </h1>
+    <div class="nav">
+      <ul>
+        <li class="check">首页</li>
+        <li>全部商品</li>
+        <li>个人中心</li>
+        <li>我的订单</li>
+        <li>专属福利</li>
+      </ul>
+    </div>
+    <div class="search">
+      <input type="text" placeholder="输入关键字">
+      <span><img src="../assets/img/search.png" alt=""></span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +25,44 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style lang="less" scoped>
+.header {
+  height: 200px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  .nav ul {
+    display: flex;
+    width: 500px;
+    justify-content: space-between;
+    .check {
+      color: #0a328e;
+      font-weight: 700;
+    }
+  }
+  .search {
+    display: flex;
+    input {
+      width: 240px;
+      height: 38px;
+      border: 1px solid #dedede;
+      border-radius: 20px 0 0 20px;
+      text-indent: 12px;
+    }
+    span {
+      width: 50px;
+      height: 40px;
+      background-color: #0a328e;
+      border-radius: 0 20px 20px 0;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+      img {
+        width: 15px;
+        height: 15px;
+      }
+    }
+  }
+}
 </style>
